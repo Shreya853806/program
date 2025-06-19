42e228d751af4b9789bc0666ecc3f804b9e00b12
Shreya853806 <shreyavkumar5344@gmail.com>
2025-06-19 15:06:48 +0530

    Create factorial.py
    factorial of numbers

---
+++ factorial.py
@@ -0,0 +1,11 @@
+# Input number
+n = 5
+
+# Initialize factorial to 1
+factorial = 1
+
+# Calculate factorial using a loop
+for i in range(1, n + 1):
+    factorial *= i
+
+print("Factorial of", n, "is", factorial)
